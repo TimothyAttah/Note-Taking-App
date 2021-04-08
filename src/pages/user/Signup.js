5237e8c73c74fa03ec2e8200df3fd3f4901274e6
Timothy Attah <timothyattah733@gmail.com>
2021-04-08 17:49:22 +0100

    Added images.js file

--- src/pages/user/Signup.js
+++ src/pages/user/Signup.js
@@ -4,10 +4,9 @@ import React from 'react'
 import Backdrop from '../../Components/Backdrop'
 import Modal from '../../Components/modal/Modal'
 import Nav from '../../Components/nav/Nav'
-import google from '../../images/google-icon.jpg';
-import line from '../../images/Line.png';
 import SignupForm from '../../Components/forms/SignupForm'
 import { Link } from 'react-router-dom'
+import { images } from '../../Components/Images';
 
 
 
@@ -22,13 +21,13 @@ const Signup = () => {
         <h3>Create <br /> your account</h3>
         <small>Enter your personal details to start your journey today</small>
         <Button variant='outlined'>
-          <img src={ google } alt='' width='30px' />
+          <img src={ images.googleIcon } alt='' width='30px' />
           <span>Log in with Google</span>
         </Button>
         <div>
-          <img src={ line } alt='' />
+          <img src={ images.lineIcon } alt='' />
           <span>or</span>
-          <img src={ line } alt='' />
+          <img src={ images.lineIcon } alt='' />
         </div>
         <SignupForm />
         <small>Already have an account? <Link to='/user/signin'>Signin here</Link></small>
