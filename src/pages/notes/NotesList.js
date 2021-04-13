590fcc7689466e7c3b03398303b6bc279eb51247
Timothy Attah <timothyattah733@gmail.com>
2021-04-13 17:25:19 +0100

    Added Authentication to user get all notes

--- src/pages/notes/NotesList.js
+++ src/pages/notes/NotesList.js
@@ -51,7 +51,7 @@ const NotesList = () => {
       {notes.length ? (
         notes.map( note => {
           return (
-            <CardWrapper key={ note.id }>
+            <CardWrapper key={ note._id }>
               <Card>
                 <CardHeader
                   avatar={
@@ -79,7 +79,7 @@ const NotesList = () => {
                   <IconButton aria-label="share">
                     <Share />
                   </IconButton>
-                  <Link to={`/user/notes/read/${note.id}`}>
+                  <Link to={`/user/notes/read/${note._id}`}>
                     <Button variant='contained' size='small' color='primary'>
                       Read More...
                     </Button>
--- src/redux/reducer/notesReducer.js
+++ src/redux/reducer/notesReducer.js
@@ -1,28 +1,27 @@
 import { CREATE_NOTE, READ_NOTE, UPDATE_NOTE, DELETE_NOTE, GET_NOTES } from '../type';
-import {v4} from 'uuid'
 
 const initialState = {
   notes: [
-    {
-      id: v4(),
-      title: 'Merry Christmas',
-      content: 'Christmas is a wonderful things to celebrate'
-    },
-    {
-      id: v4(),
-      title: 'February Born',
-      content: 'To be born in February is a blessing'
-    },
-    {
-      id: v4(),
-      title: 'Working & Learning',
-      content: 'I am a proud student who have an amazing teacher. And I am working with him as well.'
-    },
-    {
-      id: v4(),
-      title: 'God Gift',
-      content: 'God gifts is thing of blessing from God. Use it well'
-    },
+    // {
+    //   id: v4(),
+    //   title: 'Merry Christmas',
+    //   content: 'Christmas is a wonderful things to celebrate'
+    // },
+    // {
+    //   id: v4(),
+    //   title: 'February Born',
+    //   content: 'To be born in February is a blessing'
+    // },
+    // {
+    //   id: v4(),
+    //   title: 'Working & Learning',
+    //   content: 'I am a proud student who have an amazing teacher. And I am working with him as well.'
+    // },
+    // {
+    //   id: v4(),
+    //   title: 'God Gift',
+    //   content: 'God gifts is thing of blessing from God. Use it well'
+    // },
   ]
 }
 
@@ -35,7 +34,8 @@ const notesReducer = (state = initialState, action) => {
       };
     case GET_NOTES:
       return {
-        ...state
+        ...state,
+        notes: action.payload
       }
     case READ_NOTE:
       return {
